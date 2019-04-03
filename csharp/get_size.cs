10e5297968f2c919c6aaafdd94bad372aa35c746
luningcowboy <luningcowboy@gmail.com>
2019-04-03 10:15:14 +0800

    add cs code

---
+++ csharp/get_size.cs
@@ -0,0 +1,11 @@
+using System;
+namespace GetDataSizeApplication{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("size of int {0}", sizeof(int));
+        
+        }
+    
+    }
+
+}
---
+++ csharp/test_rectangle.cs
@@ -0,0 +1,29 @@
+using System;
+namespace RectangleApplication{
+    class Rectangle{
+        double length;
+        double width;
+        public void Acceptdetails(){
+            length = 4.5;
+            width = 3.5;
+        }
+        public double getArea(){
+            return length * width;
+        }
+        public void display(){
+            Console.WriteLine("Length:{0}", this.length);
+            Console.WriteLine("Width:{0}", width);
+            Console.WriteLine("Area:{0}", getArea());
+        }
+    }
+
+    class ExecuteRectangle{
+        static void Main(string[] args){
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.display();
+            Console.ReadLine();
+        }
+    }
+
+}
