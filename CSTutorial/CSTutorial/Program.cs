22eae1a81997a6d1b24d1a1e8919bf041d75492c
luningcowboy <luningcowboy@gmail.com>
2020-01-20 13:59:15 +0800

    add types.js

---
+++ CSTutorial/CSTutorial/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace TestPrograms {
+    class Test { }
+    class Test2 { }
+    public class UnitConverter {
+        int ratio;
+        public UnitConverter(int unitRatio) {
+            ratio = unitRatio;
+        }
+        public int Convert(int unit) {
+            return unit * ratio;
+        }
+    }
+}
+namespace CSTutorial
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string msg = "Hello World";
+            Console.WriteLine(msg);
+            string upMsg = msg.ToUpper();
+            Console.WriteLine(upMsg);
+            int x = 2015;
+            msg = msg + x.ToString();
+            Console.WriteLine(msg);
+
+            TestPrograms.UnitConverter feet = new TestPrograms.UnitConverter(12);
+            TestPrograms.UnitConverter mile = new TestPrograms.UnitConverter(5280);
+        }
+    }
+}
