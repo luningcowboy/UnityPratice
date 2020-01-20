01ca6eaff2f4c09a8801491007c5da522beb04f8
luningcowboy <luningcowboy@gmail.com>
2020-01-20 22:07:42 +0800

    add ref test

--- csharp2/code/6-ref-test.cs
+++ csharp2/code/6-ref-test.cs
@@ -5,9 +5,19 @@ class Test{
         p = p + 1;
         Console.WriteLine(p);
     }
+    static void swap(ref string a, ref string b){
+        string tmp = a;
+        a = b;
+        b = tmp;
+    }
     static void Main(){
         int x = 0;
         Foo(ref x);
         Console.WriteLine(x);
+        string s1 = "aaaaa";
+        string s2 = "bbbbb";
+        swap(ref s1, ref s2);
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
     }
 }
