a2036dec346e06c74e48d1582a7a7355f3d33d10
luningcowboy <luningcowboy@gmail.com>
2020-01-21 11:09:15 +0800

    add params test

---
+++ csharp2/code/8-params-test.cs
@@ -0,0 +1,15 @@
+using System;
+class Test{
+    //params参数修饰符只能修饰方法中的最后一个参数，它能够使方法接受任意数量的指定类型参数。参数类型必须声明为数
+    static int Sum(params int[] ints){
+        int sum = 0;
+        for (int i = 0; i < ints.Length; ++i){
+            sum += ints[i];
+        }
+        return sum;
+    }
+    static void Main(){
+        int total = Sum(1,2,3,4);
+        Console.WriteLine(total);
+    }
+}
