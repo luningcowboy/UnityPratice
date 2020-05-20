12b9f8006e8aa7934e3996fa76bce2190a3ce986
luningcowboy <luningcowboy@gmail.com>
2020-05-20 22:38:04 +0800

    add new code

---
+++ csharp2/learn-in-practice/c3/3-2.cs
@@ -0,0 +1,16 @@
+#region Using directives
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace Hello_World{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
