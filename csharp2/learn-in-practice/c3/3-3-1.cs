ecb3acec34cca28afbbc6d695704e079ddbf66b8
luningcowboy <luningcowboy@gmail.com>
2020-05-20 22:41:41 +0800

    add new code

---
+++ csharp2/learn-in-practice/c3/3-3-1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program{
+    static void Main(string[] args){
+        int myInteger;
+        string myString;
+        myInteger = 17;
+        myString = "\"myInteger\" is ";
+        Console.WriteLine("{0} {1}", myString, myInteger);
+        Console.ReadKey();
+    }
+}
