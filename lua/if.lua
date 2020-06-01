be0466393b2c8427767273e359d20b1e2395b971
luningcowboy <luningcowboy@gmail.com>
2020-06-01 11:14:45 +0800

    add if

---
+++ lua/if.lua
@@ -0,0 +1,20 @@
+if(0)
+then
+    print("0 is true")
+end
+
+a = 100
+if a < 20 then
+    print("a < 20")
+else
+    print("a >= 20")
+end
+
+if a < 20 then
+    print("a < 20")
+elseif a == 20 then
+    print("a == 20")
+else
+    print("a > 20")
+end
+
