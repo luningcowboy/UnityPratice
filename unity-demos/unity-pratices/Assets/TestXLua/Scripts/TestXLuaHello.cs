c80ba5864afc3e104e4b190e2697e1608af76457
luningcowboy <luningcowboy@gmail.com>
2020-06-02 19:08:32 +0800

    add XLua add XLua hello-world

---
+++ unity-demos/unity-pratices/Assets/TestXLua/Scripts/TestXLuaHello.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestXLuaHello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XLua.LuaEnv luaEnv = new XLua.LuaEnv();
+        luaEnv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+        luaEnv.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
