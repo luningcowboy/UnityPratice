3623a4a2d2bb0767f636a998942bceea043b30ee
luningcowboy <luningcowboy@gmail.com>
2020-06-05 15:05:46 +0800

    add iterator.lua

--- lua/iterator.lua
+++ lua/iterator.lua
@@ -15,7 +15,7 @@ end
 
 function square(iteratorMax, currentNum)
     if currentNum < iteratorMax
-    then
+        then
         currentNum = currentNum + 1
         return currentNum, currentNum * currentNum
     end
@@ -24,4 +24,36 @@ end
 for i, n in square, 3, 0
 do
     print(i, n)
+end
+
+function iter(a, i)
+    i = i + 1
+    local v = a[i]
+    if v then
+        return i, v
+    end
+end
+
+function ipairs(a)
+    return iter, a, 0
+end
+
+-- 多状态迭代器
+arr = {"Google", "Baidu"}
+
+function elementIterator(collection)
+    local index = 0
+    local count = #collection
+
+    return function()
+        index = index + 1
+        if index <= count then
+            return collection[index]
+        end
+    end
+end
+
+for ele in elementIterator(arr)
+do
+    print(ele)
 end
