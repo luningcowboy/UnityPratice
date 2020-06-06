35150739962aef277083a7df63d085e8c9d6903d
luningcowboy <luningcowboy@gmail.com>
2020-06-06 10:54:57 +0800

    add new code

---
+++ lua/module/main.lua
@@ -0,0 +1,13 @@
+function loadTest1()
+    require("module")
+    print(module.constant)
+    module.func3()
+end
+function loadTest2()
+    local m = require("module")
+    print(m.constant)
+    m.func3()
+end
+
+loadTest1()
+loadTest2()
