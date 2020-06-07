4ed02851de1e9b5b364f34b07969fe509810a2b0
luningcowboy <luningcowboy@gmail.com>
2020-06-07 18:16:50 +0800

    new code

--- lua/metable.lua
+++ lua/metable.lua
@@ -6,8 +6,51 @@ setmetatable(mytable, mymetatable)
 print(getmetatable(mytable))
 
 -- __index 元方法
-
+-- 表示对表访问
 other = {foo = 3}
 t = setmetatable({}, {__index = other})
 print(t.foo) --> 3
 print(t.bar) --> nil
+
+mytable = setmetatable({key1 = "value1"},{
+        __index = function(mytable, key)
+            if key == 'key2' then
+                return 'metatablevlaue'
+            else
+                return nil
+            end
+        end
+})
+print(mytable.key1, mytable.key2)
+
+-- __newindex 元方法
+-- 用来对表更新
+mymetatable = {}
+mytable = setmetatable({key1 = "value1"}, {__newindex = mymetatable})
+print(mytable.key1)
+
+mytable.newkey = "新增2"
+print(mytable.newkey, mymetatable.newkey)
+
+mytable.key1 = "新增1"
+print(mytable.key1, mymetatable.key1)
+
+mytable = setmetatable({key1 = "value1"},{
+    __newindex = function(mytable, key, value)
+        rawset(mytable, key, "\""..value.."\"")
+    end
+})
+mytable.key1 = "new value"
+mytable.key2 = 4
+print(mytable.key1, mytable.key2)
+
+-- 为表添加操作符
+function table_maxn(t)
+    local mn = 0
+    for k, v in pairs(t) do
+        if mn < k then
+            mn = l
+        end
+    end
+    return mn
+end
