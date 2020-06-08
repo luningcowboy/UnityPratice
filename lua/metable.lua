30da9785c3038354ea3ab5bf101a59423f959d42
luningcowboy <luningcowboy@gmail.com>
2020-06-08 09:38:39 +0800

    add new code

--- lua/metable.lua
+++ lua/metable.lua
@@ -20,7 +20,7 @@ mytable = setmetatable({key1 = "value1"},{
                 return nil
             end
         end
-})
+    })
 print(mytable.key1, mytable.key2)
 
 -- __newindex 元方法
@@ -36,10 +36,10 @@ mytable.key1 = "新增1"
 print(mytable.key1, mymetatable.key1)
 
 mytable = setmetatable({key1 = "value1"},{
-    __newindex = function(mytable, key, value)
-        rawset(mytable, key, "\""..value.."\"")
-    end
-})
+        __newindex = function(mytable, key, value)
+            rawset(mytable, key, "\""..value.."\"")
+        end
+    })
 mytable.key1 = "new value"
 mytable.key2 = 4
 print(mytable.key1, mytable.key2)
@@ -49,8 +49,66 @@ function table_maxn(t)
     local mn = 0
     for k, v in pairs(t) do
         if mn < k then
-            mn = l
+            mn = k
         end
     end
     return mn
 end
+
+mytable = setmetatable({1, 2, 3}, {
+        __add = function(mytable, newtable)
+            for i = 1, table_maxn(newtable) do
+                table.insert(mytable, table_maxn(mytable) + 1, newtable[i])
+            end
+            return mytable
+        end
+})
+
+secondtable = {4, 5, 6}
+
+mytable = mytable + secondtable
+for k, v in ipairs(mytable) do
+    print(k, v)
+end
+
+--[[
+__add +
+__sub -
+__mul *
+__div /
+__mod %
+__unm -
+__concat ..
+_eq ==
+__lt <
+__le <=
+--]]
+
+-- __call元方法
+
+mytable = setmetatable({10}, {
+        __call = function(mytable, newtable)
+            sum = 0
+            for i = 1, table_maxn(mytable) do
+                sum = sum + mytable[i]
+            end
+            for i = 1, table_maxn(newtable) do
+                sum = sum + newtable[i]
+            end
+            return sum
+        end
+    })
+newtable = {10, 20, 30}
+print(mytable(newtable))
+
+-- __tostring 元方法
+mytable = setmetatable({10, 20, 30}, {
+        __tostring = function(mytable)
+            sum = 0
+            for k, v in pairs(mytable) do
+                sum = sum + v
+            end
+            return "sum = " .. sum
+        end
+    })
+print(mytable)
