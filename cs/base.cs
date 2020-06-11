46e5437f411b279c0a559165f5e7ea49316707db
luningcowboy <luningcowboy@gmail.com>
2020-06-11 09:42:44 +0800

    add new code

---
+++ cs/base.cs
@@ -0,0 +1,20 @@
+using System;
+class Program{
+    static void Main(string[] args){
+        int num1 = 100;
+        double num2 = 100.23;
+        bool isFlag = true;
+        string name = "hello";
+
+        Console.WriteLine("num1 = " + num1);
+        Console.WriteLine("num2 = " + num2);
+        Console.WriteLine("isFlag = " + isFlag);
+        Console.WriteLine("name = " + name);
+
+        var num3 = 101;
+        var num4 = 1.12;
+        var isEnd = false;
+        var world = "world";
+        Console.WriteLine("world = " + world);
+    }
+}
