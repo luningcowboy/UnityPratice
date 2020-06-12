fd894ea82ea3e754b5a88719fa894adefc7797be
luningcowboy <luningcowboy@gmail.com>
2020-06-12 15:00:16 +0800

    new code

--- cs/base.cs
+++ cs/base.cs
@@ -1,6 +1,23 @@
 using System;
 class Program{
-    static void Main(string[] args){
+    static void Switch1(out int a, out int b){
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+    static void Switch2(out int a, out int b)
+    {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+    }
+    static void Switch3(out int a, out int b)
+    {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+    }
+    static void Test1(){
         int num1 = 100;
         double num2 = 100.23;
         bool isFlag = true;
@@ -16,5 +33,11 @@ class Program{
         var isEnd = false;
         var world = "world";
         Console.WriteLine("world = " + world);
+    }
+    static void Main(string[] args){
+        int a = 100;
+        int b = 200;
+        Switch1(out a, out b);
+        Console.WriteLine($"a = {a},b = {b}");
     }
 }
