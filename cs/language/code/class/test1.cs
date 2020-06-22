c09e2c5dd1e7bd4b9c169b659da382f5ae385d6f
luningcowboy <luningcowboy@gmail.com>
2020-06-22 15:35:51 +0800

    add new code

---
+++ cs/language/code/class/test1.cs
@@ -0,0 +1,14 @@
+namespace code_1
+{
+    class Test
+    {
+        private int id;
+        public readonly string name;
+        internal static int age;
+        private const string major = "计算机";
+        private void PrintMsg()
+        {
+            Console.WriteLine("编号:" + id);
+        }
+    }
+}
