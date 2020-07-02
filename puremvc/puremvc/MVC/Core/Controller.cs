dedfaf9f8527bb05f878dac855fe709674769262
luningcowboy <luningcowboy@gmail.com>
2020-07-02 17:46:05 +0800

    add new code

--- puremvc/puremvc/MVC/Core/Controller.cs
+++ puremvc/puremvc/MVC/Core/Controller.cs
@@ -24,5 +24,50 @@ namespace MVC.Core
         {
             return InstanceMap.GetOrAdd(key, new Lazy<IController>(factory(key))).Value;
         }
+
+        public virtual void ExecuteCommand(INotification notification)
+        {
+            if(commandMap.TryGetValue(notification.Name, out var factory))
+            {
+                var commandInstance = factory();
+                commandInstance.InitializeController(multitonKey);
+                commandInstance.Execute(notification);
+            }
+        }
+
+        public virtual void RegisterCommand(string notificationName, Func<ICommand> factory)
+        {
+            if(commandMap.TryGetValue(notificationName, out _) = false)
+            {
+                view.RegisterObserver(notificationName, new Observer(ExecuteCommand, this));
+                commandMap[notificationName] = factory;
+            }
+        }
+
+        public virtual void RemoveCommand(string notificationName)
+        {
+            if(commandMap.TryRemove(notificationName, out _))
+            {
+                view.RemoveObserver(notificationName, this);
+            }
+        }
+
+        public virtual bool HasCommand(string notificationName)
+        {
+            return commandMap.ContainsKey(notificationName);
+        }
+
+        public static void RemoveComtroller(string key)
+        {
+            InstanceMap.TryRemove(key, out _);
+        }
+
+        protected IView view;
+
+        protected readonly string nultitonKey;
+
+        protected readonly ConcurrentDictionary<string, Func<ICommand>> commandMap;
+
+        protected static readonly ConcurrentDictionary<string, Lazy<InitializeController>> InstanceMap = new ConcurrentDictionary<string, Lazy<InitializeController>>();
     }
 }
