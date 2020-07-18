8a04090d7dda141f16b53618acde2baed02111f2
luningcowboy <luningcowboy@gmail.com>
2020-07-18 10:23:08 +0800

    add mvc

---
+++ framework/project/base/XScripts/Frameworks/Base/MVC/Patterns/Observer/Notification.lua
@@ -0,0 +1,12 @@
+local Notification = Class("Notification",{})
+
+function Notification:Ctor(name, body, type)
+end
+
+function Notification:GetName() end
+function Notification:SetBody() end
+function Notification:GetBody() end
+function Notification:SetType() end
+function Notification:GetType() end
+function Notification:ToString() end
+return Notification
