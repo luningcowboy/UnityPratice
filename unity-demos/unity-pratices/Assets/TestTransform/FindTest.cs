02d044a0d99be2681acc8f900a87a9afca3e1dae
luning <luningcowboy@gmail.com>
2020-08-05 16:17:01 +0800

    Add: TestTransform

---
+++ unity-demos/unity-pratices/Assets/TestTransform/FindTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindTest : MonoBehaviour
+{
+    public GameObject objectYouWant;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestFindByName();
+        TestFindByTag();
+    }
+    private void TestFindByName()
+    {
+        var objCube = GameObject.Find("Cube");
+        if(objCube != null)
+        {
+            print("find cube game object success");
+        }
+        else
+        {
+            print("find cube game object failed");
+        }
+
+    }
+    private void TestFindByTag()
+    {
+        var objCube = GameObject.FindWithTag("TagCube");
+        if(objCube != null)
+        {
+            print("find cube game object success");
+        }
+        else
+        {
+            print("find cube game object failed");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ unity-demos/unity-pratices/Assets/TestTransform/TargetCubeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCubeScript : MonoBehaviour
+{
+    private GameObject CubeObj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CubeObj = GameObject.Find("Cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CubeObj.transform.Translate(.1f, 0f, 0f);
+        CubeObj.transform.Rotate(0f, 0f , 1f);
+        CubeObj.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+    }
+}
