b1349ce4f0dc19c082fd20d11e0dcb9eb9af85f5
luning <luningcowboy@gmail.com>
2020-08-08 13:30:37 +0800

    add test ray

---
+++ unity-demos/unity-pratices/Assets/TestRay/RaycastScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dirX = Input.GetAxis("Mouse X");
+        var dirY = Input.GetAxis("Mouse Y");
+        transform.Rotate(dirY, dirX, 0);
+        CheckForRaycastHit();
+    }
+
+    void CheckForRaycastHit()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            print(hit.collider.gameObject.name + "destroy");
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
