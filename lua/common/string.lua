a2ed49275c8d9c262a918e962e6354339d2e6f5e
luning <luningcowboy@gmail.com>
2020-08-10 21:13:56 +0800

    add new string.split2

--- lua/common/string.lua
+++ lua/common/string.lua
@@ -21,7 +21,30 @@ local function split(str, pattern)
 
     return ret
 end
-local xx = split('a,b,c', ',')
+local function split2(str, pattern)
+    if not pattern then
+        pattern = ','
+    end
+    local ret = {}
+    local start = 1
+    local find = 1
+    while true do
+        local last = string.find(str, pattern, start)
+        if not last then
+            ret[find] = string.sub(str, start, string.len(str))
+            break
+        end
+
+        ret[find] = string.sub(str, start, last - 1)
+        start = last + string.len(pattern)
+        find = find + 1
+
+    end
+    return ret
+end
+--local xx = split('a,b,c', ',')
+--local xx = split2('a', ',')
+local xx = split('a', ',')
 for _, x in pairs(xx) do
     print(x)
 end
