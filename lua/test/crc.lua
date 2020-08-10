1c287af28c770da287cf2b0b71dee44c9a5264bf
luning <luningcowboy@gmail.com>
2020-08-10 13:27:49 +0800

    add new code

--- lua/test/crc.lua
+++ lua/test/crc.lua
@@ -12,5 +12,29 @@ local function GetCrcCode(bytes)
     return hash & 0x7FFFFFFF
 end
 
-local testData = {}
-print(GetCrcCode(testData))
+local function cond2int(x)
+    return math.tointeger(x) or x
+end
+
+local function GetCrcCode2(bytes)
+    local hash = 0
+    local x = 0
+    for i = 1, string.len(bytes) do
+        local v = string.sub(bytes, i, i)
+        v = string.byte(v)
+        print("XXXX", math.tointeger(hash << 4) )
+        hash = (hash << 4) + v
+        print("YYYY", hash)
+        x = math.tointeger(hash & 0xF0000000)
+        print(i, v, hash, x)
+        if x ~= 0 then
+            hash = hash ~ (x >> 24)
+            print('hash:', hash)
+            hash = hash & (~x)
+            print('hash:', hash)
+        end
+    end
+    return hash & 0x7FFFFFFF
+end
+
+print(GetCrcCode2('33DH6DDE62D2$4E03BE41-641D-5C99-B378-89556BDB4E6C'))
--- lua/test/t1.lua
+++ lua/test/t1.lua
@@ -3,3 +3,5 @@ print("HelloWorld")
 function norm(x, y)
     return math.sqrt(x^2 + y^2)
 end
+
+print(913881732 & 0xF0000000)
