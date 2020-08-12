ad2f76b9f78ac37bb696abc98e81cf2b7a5310f5
luning <luningcowboy@gmail.com>
2020-08-12 11:53:38 +0800

    add join and format

--- lua/common/string.lua
+++ lua/common/string.lua
@@ -1,4 +1,5 @@
-local function split(str, pattern)
+-- 这个算法只能分割pattern=单字符
+local function split2(str, pattern)
     if not pattern then
         pattern = ','
     end
@@ -7,7 +8,6 @@ local function split(str, pattern)
     local sub_str = ''
     for i = 1, len do
         local tmp_str = string.sub(str, i, i)
-        print(i, tmp_str)
         if tmp_str ~= pattern then
             sub_str = sub_str .. tmp_str
         else
@@ -21,7 +21,7 @@ local function split(str, pattern)
 
     return ret
 end
-local function split2(str, pattern)
+local function split(str, pattern)
     if not pattern then
         pattern = ','
     end
@@ -42,9 +42,32 @@ local function split2(str, pattern)
     end
     return ret
 end
---local xx = split('a,b,c', ',')
---local xx = split2('a', ',')
-local xx = split('a', ',')
-for _, x in pairs(xx) do
-    print(x)
+local function format(list)
+    local ret = ""
+    for _, str in ipairs(list) do
+        ret = ret .. str
+    end
+    return ret
+end
+local function join(list, pattern)
+    local ret = ""
+    local len = #list
+    local str = ""
+    for i = 1, len  do
+        str = list[i]
+        if i < len then
+            ret = ret .. str .. pattern
+        else
+            ret = ret .. str
+        end
+    end
+    return ret
+end
+
+local function testFormate()
+    print(format({"a",1,2,3,"b"}))
+end
+local function testJoin()
+    print(join({"a",1,2,3,"b"}, "xx"))
+    print(join({"a"}, "xx"))
 end
