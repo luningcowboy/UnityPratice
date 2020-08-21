ead275523b04cfdbfc83adbaa99ad01b59cf4c92
luning <luningcowboy@gmail.com>
2020-08-21 15:21:22 +0800

    add new test

---
+++ unity-demos/unity-pratices/Assets/TestMonoBehaviour/TestMonoBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMonoBehaviour : MonoBehaviour
+{
+    private void Awake()
+    {
+        print("Awake");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Start");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Update");
+    }
+
+    private void FixedUpdate()
+    {
+        print("FixedUpdate");
+        
+    }
+
+    private void LateUpdate()
+    {
+        print("LateUpdate");
+        
+    }
+
+    private void OnGUI()
+    {
+        print("OnGUI");
+        
+    }
+
+    private void OnDestroy()
+    {
+        print("OnDestory");
+        
+    }
+
+    private void OnEnable()
+    {
+        print("OnEnable");
+        
+    }
+}
