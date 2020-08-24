ab7e8ab0acad88bec0f404375a3d46a092bee0fc
luning <luningcowboy@gmail.com>
2020-08-24 21:55:24 +0800

    [Odin]add test AssesOnlyAttribute

---
+++ unity-demos/unity-pratices/Assets/OdinTest/1. TestAssetsOnlyAttribute/TestAssetsOnlyAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class TestAssetsOnlyAttribute : MonoBehaviour
+{
+    [AssetsOnly]
+    public List<GameObject> OnlyPrefabs;
+
+    [AssetsOnly]
+    public GameObject SomePrefab;
+
+    [AssetsOnly]
+    public Material MaterialAsset;
+
+    [AssetsOnly]
+    public List<GameObject> SomeMeshRenderOnPrefab;
+
+    [SceneObjectsOnly]
+    public List<GameObject> OnlySceneObjects;
+
+    [SceneObjectsOnly]
+    public GameObject SomeSceneObject;
+
+    [SceneObjectsOnly]
+    public MeshRenderer SomeMeshRender;
+
+}
