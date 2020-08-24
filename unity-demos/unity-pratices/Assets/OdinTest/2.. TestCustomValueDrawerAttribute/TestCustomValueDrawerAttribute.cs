94dfc59cb1e1212806b39b0ef1b5fcf095e0d82c
luning <luningcowboy@gmail.com>
2020-08-24 22:08:16 +0800

    [Odin] add TestCustomValueDrawerAttribute

---
+++ unity-demos/unity-pratices/Assets/OdinTest/2.. TestCustomValueDrawerAttribute/TestCustomValueDrawerAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEditor.Experimental.Networking.PlayerConnection;
+using UnityEngine;
+using UnityEditor;
+using EditorGUILayout = UnityEditor.EditorGUILayout;
+
+public class TestCustomValueDrawerAttribute : MonoBehaviour
+{
+    [CustomValueDrawer("HaveLabelNameFunction")]
+    public string HaveLableName;
+
+    [CustomValueDrawer("NoLabelNameFunction")]
+    public string NoLableName;
+
+    public string HaveLabelNameFunction(string tmpName, GUIContent label)
+    {
+        return EditorGUILayout.TextField(label,tmpName);
+    }
+
+    public string NoLabelNameFunction(string tmpName, GUIContent label)
+    {
+        return EditorGUILayout.TextField(label, tmpName);
+    }
+}
