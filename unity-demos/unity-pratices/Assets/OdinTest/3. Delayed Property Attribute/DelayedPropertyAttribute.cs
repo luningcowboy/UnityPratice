2f194d1fae786b019f206edabb87a9ff7d7d33f1
luning <luningcowboy@gmail.com>
2020-08-26 13:47:54 +0800

    add new code

---
+++ unity-demos/unity-pratices/Assets/OdinTest/3. Delayed Property Attribute/DelayedPropertyAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class DelayedPropertyAttribute : MonoBehaviour
+{
+    [OnValueChanged("ValueChangeCallback")]
+    public int field;
+
+    [ShowInInspector]
+    [OnValueChanged("ValueChangeCallback")]
+    public string property { get; set; }
+    
+    [Delayed]
+    [OnValueChanged("ValueChangeCallback")]
+    public int fieldDelay;
+    
+    [ShowInInspector, Sirenix.OdinInspector.DelayedProperty]
+    [OnValueChanged("ValueChangeCallback")]
+    public string propertyDelay { get; set; }
+    
+
+    public void ValueChangeCallback()
+    {
+        Debug.Log("ValueChangeCallback");
+    }
+
+
+
+}
