1a0a934eb25ae29c35edd9a60e3259178b32b582
luning <luningcowboy@gmail.com>
2020-08-27 13:18:08 +0800

    add UGUI

---
+++ UGUI/Assets/Scripts/ImageTestRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class ImageTestRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.forward * 120 * Time.deltaTime);
+        
+    }
+}
