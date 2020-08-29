90741f7068ec8656a24517768116da47aa577062
luning <luningcowboy@gmail.com>
2020-08-29 14:08:15 +0800

    add odin test

---
+++ unity-demos/unity-pratices/Assets/OdinTest/4.DetailedInfoBox/TestDetailedInfoBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class TestDetailedInfoBox : MonoBehaviour
+{
+    [DetailedInfoBox("详情请点击", "xxxxxxxxxxx")]
+    public string message = "无";
+
+    [DetailedInfoBox("详情请点击", "", InfoMessageType.Info)]
+    public string InfoMessage = "null";
+
+    [DetailedInfoBox("详情请点击", "", InfoMessageType.Warning)]
+    public string WarningMessage = "";
+
+    [DetailedInfoBox("详情请点击", "", InfoMessageType.Error)]
+    public string ErrorMessage = "";
+    [DetailedInfoBox("详情请点击", "", InfoMessageType.Error, VisibleIf="VisibleFunction")]
+    public string VisibleMessage = "";
+    [DetailedInfoBox("详情请点击", "", InfoMessageType.Error, VisibleIf="NoVisibleFunction")]
+    public string NoVisibleMessage = "";
+
+    public bool VisibleFunction()
+    {
+        return true;
+    }
+
+    public bool NoVisibleFunction()
+    {
+        return false;
+    }
+}
