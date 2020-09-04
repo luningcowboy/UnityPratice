27dcbdf6982371b9f83baa6d566e4ae30d7515c6
luning <luningcowboy@gmail.com>
2020-09-04 18:09:16 +0800

    添加多个返回值测试

--- lua/test/test_func.lua
+++ lua/test/test_func.lua
@@ -1,4 +1,14 @@
 local function func1()
 end
+--[[
 func1.__call = function() print("new") end
 print(func1)
+]]
+
+local function func2()
+    return 1, 2
+end
+
+local a, b = func2()
+print(a, b)
+
