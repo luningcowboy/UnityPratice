3a23a633bd795f4e991330993674e2de0221caca
luning <luningcowboy@gmail.com>
2020-09-05 16:18:47 +0800

    实现一个记录长度的字典，不同每次通过遍历获取长度

---
+++ lua/test/dic/dic.lua
@@ -0,0 +1,37 @@
+local Dic = {}
+local _M = {
+    __call = function()
+        local dic = {}
+        local len = 0
+        local m = {
+            __len = function(t) 
+                return len
+            end,
+            __newindex = function(t,k,v)
+                len = len + 1
+                rawset(t,k,v)
+            end
+        }
+        setmetatable(dic, m)
+        return dic
+    end
+}
+setmetatable(Dic, _M)
+--[[
+--Test
+local a = Dic()
+local b = Dic()
+print("a:" .. #a)
+a.a = 1
+a.b = nil
+a.c = ""
+print("a:" .. #a)
+for i = 1, 10 do
+    b[#b+1] = i
+end
+b[#b+1] = nil
+b.a = nil
+print("b:" .. #b)
+]]
+
+return Dic
