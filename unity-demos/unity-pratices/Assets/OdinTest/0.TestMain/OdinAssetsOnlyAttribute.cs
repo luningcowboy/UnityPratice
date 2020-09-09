478378e3bbb2f3a0bd19133d28a5b80bee116010
luning <luningcowboy@gmail.com>
2020-09-09 21:06:21 +0800

    add new code

--- unity-demos/unity-pratices/Assets/OdinTest/0.TestMain/OdinAssetsOnlyAttribute.cs
+++ unity-demos/unity-pratices/Assets/OdinTest/0.TestMain/OdinAssetsOnlyAttribute.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour, IPointerDownHandler, IPointerClickHandler
+, IPointerEnterHandler, IPointerExitHandler
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,25 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        print("OnPointerDown");
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("OnPointerClick");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("OnPointerEnter"); 
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("OnPointerExit"); 
     }
 }
---
+++ unity-demos/unity-pratices/Assets/TestDrag/TestDrag.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestDrag : MonoBehaviour, 
+    IPointerClickHandler, 
+    IPointerEnterHandler, 
+    IPointerExitHandler,
+    IDragHandler,
+    IBeginDragHandler,
+    IEndDragHandler
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print($"OnPointerClick{eventData.position.x} {eventData.position.y}");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print($"OnPointerEnter{eventData.position.x} {eventData.position.y}");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print($"OnPointerExit{eventData.position.x} {eventData.position.y}");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print($"OnDrag{eventData.position.x} {eventData.position.y}");
+        transform.position = eventData.position;
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        print($"OnBeginDrag{eventData.position.x} {eventData.position.y}");
+        transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        print($"OnEndDrag{eventData.position.x} {eventData.position.y}");
+    }
+}
