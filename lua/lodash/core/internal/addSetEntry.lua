2adc564a5c90c9c9f2a873975d62873c65a11e29
luning <luningcowboy@gmail.com>
2020-09-15 17:11:24 +0800

    add new code

---
+++ lua/lodash/core/internal/addSetEntry.lua
@@ -0,0 +1,3 @@
+local function addSetEntry(set, value)
+end
+return addSetEntry
--- lua/lodash/core/lodash.lua
+++ lua/lodash/core/lodash.lua
@@ -1,6 +1,7 @@
 print('load lodash start')
 lodash = {}
 lodash.split = require 'core.split'
-
 lodash.chunk = require 'core.chunk'
+lodash.after = require 'core.after'
+lodash.before = require 'core.before'
 print('load lodash end')
