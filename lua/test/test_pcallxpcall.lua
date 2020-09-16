e09b328ee18ee8024bf53202fb3e65ebae000afc
luning <luningcowboy@gmail.com>
2020-09-16 11:58:55 +0800

    add new code

--- lua/test/test_pcallxpcall.lua
+++ lua/test/test_pcallxpcall.lua
@@ -5,6 +5,9 @@ end
 local function func2()
     print("func2")
 end
+local function func3()
+    return "hello world"
+end
 
 if pcall(func2) then
     print("xxxx")
@@ -14,6 +17,9 @@ if not pcall(func1) then
     print("func2 failed")
 end
 
+local r1, r2 = pcall(func3)
+print("pcall", r1, r2)
+
 --local ret, err = xpcall(func2)
 --print(ret, err)
 local function err_handler(err)
@@ -22,7 +28,7 @@ local function err_handler(err)
 end
 
 local stat, ret = xpcall(func1, err_handler)
-print(stat, ret)
+print("xpcall", stat, ret)
 
 stat, ret = xpcall(func2, err_handler)
-print(stat, ret)
+print("xpcall", stat, ret)
